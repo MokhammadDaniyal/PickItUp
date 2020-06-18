0cbfc45238f72c7e1c4e25af74eaedc931be99e0
Daniyal <mokhammad.daniyal@gmail.com>
2020-06-17 18:56:06 -0700

    Init commit

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float duration;    //the max time of a walking session (set to ten)
+    float elapsedTime = 0f; //time since started walk
+    float wait = 0f; //wait this much time
+    float waitTime = 0f; //waited this much time
+
+    float randomX;  //randomly go this X direction
+    float randomZ;  //randomly go this Z direction
+
+    bool move = true; //start moving
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        randomX = Random.Range(-20, 20);
+        randomZ = Random.Range(-20, 20);
+    }
+
+    void FixedUpdate()
+    {
+
+
+
+        Debug.Log (elapsedTime);
+
+        if (elapsedTime < duration && move)
+        {
+            //if its moving and didn't move too much
+            //transform.Translate(new Vector3(randomX, 0, randomZ) * Time.deltaTime);
+
+            rb.AddForce(new Vector3(randomX *2, 0, randomZ*2));
+            elapsedTime += Time.deltaTime;
+
+        }
+        else
+        {
+            //do not move and start waiting for random time
+            move = false;
+            wait = Random.Range(5, 10);
+            waitTime = 0f;
+        }
+
+        if (waitTime < wait && !move)
+        {
+            //you are waiting
+            waitTime += Time.deltaTime;
+
+
+        }
+        else
+        {
+            //done waiting. Move to these random directions
+            move = true;
+            elapsedTime = 0f;
+            randomX = Random.Range(-20, 20);
+            randomZ = Random.Range(-20, 20);
+        }
+    }
+  
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    private Rigidbody rb;
+
+    public Text countText;
+
+    float rot;
+    public float rotateAmount;
+
+    private int count;
+    // Start is called before the first frame update
+
+
+    public Text gameOver;
+
+    void RotateRight()
+    {
+        rot = rotateAmount;
+        transform.Rotate(0, rot, 0);
+    }
+
+    void RotateLeft()
+    {
+        rot = -rotateAmount;
+        transform.Rotate(0, rot, 0);
+    }
+
+    void Start()
+    {
+        gameOver.enabled = false;
+        count = 0;
+        rb = GetComponent<Rigidbody>();
+        countText.text = "Score: " + count.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetMouseButton(0))
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if ((Input.mousePosition.x - Screen.width / 2 > 0))
+            {
+                RotateRight();
+            } else
+            {
+                RotateLeft();
+            }
+
+
+        } 
+    }
+
+    void FixedUpdate()
+    {
+
+        // rb.AddForce(0,0,moveSpeed);
+        rb.velocity = transform.forward * moveSpeed;
+       // Vector3 movement = new Vector3(1.0f, 1.0f, 1.0f);
+       // rb.AddForce(movement);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            gameOver.enabled = true;
+            Time.timeScale = 0;
+
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision!");
+
+        if (other.gameObject.CompareTag("Food"))
+        {
+            count += 1;
+            other.gameObject.SetActive(false);
+            countText.text = "Score: " + count.ToString();
+
+        }
+
+
+        
+    }
+}
